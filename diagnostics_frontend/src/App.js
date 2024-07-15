5c001776a361125a18b352badcae1896e5bfc755
PaulChinta7 <paulchinta7@gmail.com>
2024-07-15 12:19:26 -0500

    basic setup

---
+++ diagnostics_frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Welcome to the diagnostics app
+    </div>
+  );
+}
+
+export default App;
