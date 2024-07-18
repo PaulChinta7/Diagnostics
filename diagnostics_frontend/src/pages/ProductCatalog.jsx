66bb52937ea893ec180b37322f0a6aec1d7ef8bd
PaulChinta7 <paulchinta7@gmail.com>
2024-07-18 15:28:45 -0500

    search and pagination added

--- diagnostics_frontend/src/pages/ProductCatalog.jsx
+++ diagnostics_frontend/src/pages/ProductCatalog.jsx
@@ -3,13 +3,19 @@ import ProductCard from "../components/ProductCard";
 import { data } from "../data";
 const ProductCatalog = () => {
     const [obj,setobj]=useState(data);
-    
-
+    const [page,setpage]=useState(1);
+    const [searchTerm,setsearchTerm]=useState(1);
+    const HandlePrev=()=>{if(page>1)setpage(page-1)};
+    const HandleNext=()=>{if(page<obj.length/6)setpage(page+1)};
+    const HandleSearch=(e)=>{console.log(e.target.value)};
     return ( <>
     <div className="bg">
-        <div className="productlist">
-            {obj.map((item,index)=><ProductCard key={index} product={item}/>)}
-
+        <div className="productlist2">
+            <div className="searchbar">search <input className="searchfield" type="text" onChange={HandleSearch} /></div>
+            <div className="productlist">
+            {obj.slice(page*6-6,page*6).map((item,index)=><ProductCard key={index} product={item}/>)}
+            </div>
+            <div className="pagination"><button className="pagebutton" onClick={HandlePrev}>Prev</button> <button className="pagebutton" onClick={HandleNext}>Next</button></div>
         </div>
         <div className="productdescription">
             
