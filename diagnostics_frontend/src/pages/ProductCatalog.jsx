dba95b3707e9508c9a16757d6d2ac23bc395e3c4
PaulChinta7 <paulchinta7@gmail.com>
2024-07-18 15:37:08 -0500

    removed bugs |next and prev buttons position

--- diagnostics_frontend/src/pages/ProductCatalog.jsx
+++ diagnostics_frontend/src/pages/ProductCatalog.jsx
@@ -4,16 +4,17 @@ import { data } from "../data";
 const ProductCatalog = () => {
     const [obj,setobj]=useState(data);
     const [page,setpage]=useState(1);
-    const [searchTerm,setsearchTerm]=useState(1);
+    const [searchTerm,setsearchTerm]=useState("");
     const HandlePrev=()=>{if(page>1)setpage(page-1)};
     const HandleNext=()=>{if(page<obj.length/6)setpage(page+1)};
-    const HandleSearch=(e)=>{console.log(e.target.value)};
+    const HandleSearch=(e)=>{setpage(1);setsearchTerm(e.target.value)};
+
     return ( <>
     <div className="bg">
         <div className="productlist2">
             <div className="searchbar">search <input className="searchfield" type="text" onChange={HandleSearch} /></div>
             <div className="productlist">
-            {obj.slice(page*6-6,page*6).map((item,index)=><ProductCard key={index} product={item}/>)}
+            {obj.filter((item)=>item.name.toLowerCase().includes(searchTerm.toLowerCase())).slice(page*6-6,page*6).map((item,index)=><ProductCard key={index} product={item}/>)}
             </div>
             <div className="pagination"><button className="pagebutton" onClick={HandlePrev}>Prev</button> <button className="pagebutton" onClick={HandleNext}>Next</button></div>
         </div>
