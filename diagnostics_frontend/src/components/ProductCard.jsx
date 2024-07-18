1559273c4f0e9d0df1e0de5c931ed1f67b0aa268
PaulChinta7 <paulchinta7@gmail.com>
2024-07-17 21:19:07 -0500

    changes in product card

--- diagnostics_frontend/src/components/ProductCard.jsx
+++ diagnostics_frontend/src/components/ProductCard.jsx
@@ -1,57 +1,25 @@
-import { useState } from "react"
-import { Link } from "react-router-dom";
-// name,
-// no,
-// description,
-// summary,
-// type,
-// price
-// location
-// reportsin
-
-
-const ProductCard = (prop) => {
-
-    const [Name,setName]=useState(prop.obj.name);
-    const [Id,setId]=useState(prop.obj.no);
-    const [Description,setDescription]=useState(prop.obj.description);
-    const [Summary,setSummary]=useState(prop.obj.summary);
-    const [Type,setType]=useState(prop.obj.type);
-    const [Price,setPrice]=useState(prop.obj.price);
-    const [Location,setLocation]=useState(prop.obj.location);
-    const [ReportsIn,setReportsIn]=useState(prop.obj.reportsin);
-
+import {Link} from 'react-router-dom'
+const ProductCard = (props) => {
+    const data=props;
+    console.log(data.product.name);
     return ( <>
-    <div className="productcard">
-        <div className="productcard-data">
-        <div className="productcard-line">
-        <span>{Name}</span>
-        <span>{Id}</span>
+    
+    <div className="productcard"> 
+        <div className="productcardone">
+        <p> {data.product.name}</p>
+        <p>{data.product.no}</p>
         </div>
-        <hr/>
-        <span>{Description}</span>
-      
-        <div className="productcard-line">
-        <span>{Type}</span>
-        <span>{Location}</span>
-        <span>{ReportsIn}</span>
-        <span>${Price}</span>
+        <div className="productcardtwo">
+        <p>{data.product.description}</p><span><Link>more..</Link></span>    
         </div>
-        <div className="productcard-line"> 
-        <Link to="/products">more...</Link>
-        <button className="button"> Add</button>
+        <div className="productcardthree">
+        <p>{data.product.type}</p>
+        <p>{data.product.location}</p>
+        <p>${data.product.price}</p>
+        <button className="addbutton">Add</button>
         </div>
 
-
         </div>
-        {/* <span>TestNo.</span>
-        <span>Test description</span>
-        <span>Test Type</span>
-        <span>Test price</span>
-        <span>Test location</span>
-        <span>Reports in</span>
-        <span>Summary</span> */}
-    </div>
     </> );
 }
  
--- diagnostics_frontend/src/pages/ProductCatalog.jsx
+++ diagnostics_frontend/src/pages/ProductCatalog.jsx
@@ -2,15 +2,22 @@ import {  useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { data } from "../data";
 const ProductCatalog = () => {
-    // data for products
-
-
-
-   
+    const [obj,setobj]=useState(data);
+    
 
     return ( <>
     <div className="bg">
-     <p>This is Product page</p>
+        <div className="productlist">
+            {obj.map((item,index)=><ProductCard key={index} product={item}/>)}
+
+        </div>
+        <div className="productdescription">
+            
+        </div>
+   
+    
+
+
     </div>
     </> );
 }
