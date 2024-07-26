adb1f9abf1d0dc70bf515addc30f69a639dbad84
PaulChinta7 <paulchinta7@gmail.com>
2024-07-25 23:47:42 -0500

    changes css

--- diagnostics_frontend/src/pages/Account.jsx
+++ diagnostics_frontend/src/pages/Account.jsx
@@ -6,54 +6,28 @@ const Account = () => {
 
     </div>
     <div className="accountdetails">
+
         <div className="personaldetails">
             <div className="personaldetailsname">
-            <span>firstname: <input type="text" placeholder="Preetham Paul"/></span>
-            <span>Lastname: <input type="text" placeholder="Chinta" /></span>
-            </div>
-            <div className="personaldetailsage">
-            <span>Date of birth: <input type="date" /></span>
-            <span>age: <input type="text" placeholder="16" /></span> 
-           
-            </div>
-            <div className="personaldetailsage2">
-            <span>Gender: <select name="" id="">
-                <option value="">Male</option>
-                <option value="">Female</option>
-                </select></span> 
-            <span>blood type
-                <select name="" id="">
-                    <option value="">O+</option>
-                    <option value="">AB+</option>
-                    <option value="">A+</option>
-                    <option value="">B+</option>
-                    <option value="">AB-</option>
-                    <option value="">O-</option>
-                </select>
-                </span> 
+                <div className="inputlabel">
+                <span className="f10 py5">Firstname</span> <input type="text" placeholder="Preetham Paul"/>
+                </div>
+                <div className="inputlabel">
+                <span className="f10 py5">Lastname</span> <input type="text" placeholder="Chinta" />
+                </div>
             </div>
 
-            <div className="personaldetailsaddress">
-            <span>Address <input type="text" /></span> 
-            <span>City </span> 
-            <span>State</span> 
-            <span>Pincode</span> 
-            <span>Country</span> 
+
+
+
+            <div className="personaldetailsage">
+            <div className="inputlabel"><span className="f10 py5">Date of birth</span> <input type="date" /></div>
+            <div className="inputlabel"> <span className="f10 py5"> Age</span> <input  type="text" placeholder="16" /></div> 
+           
             </div>
+
         </div>
-        <div className="userdetails">
-            <span>Email</span>
-            <span>Phone</span>
-            <span>Password</span>
-            <span>Confirm Password</span>
-        </div>
-        <div className="billingdetails">
-            <span>card no.</span>
-            <span>expiry</span>
-            <span>cvv</span>
-            <span>Name</span>
-            <span>Address</span>
-        </div>
+      
     </div>
 
         </div>
